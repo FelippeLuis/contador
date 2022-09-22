150ff74ef4cf2b2e4342d2b313d19ce821d54e92
Felippe Luis da Silva Schiesser <felippeschiesser@gmail.com>
2022-09-22 15:30:52 -0300

    aaa

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+let numero = 0
+
+function aumentar() {
+    numero = numero + 1
+    // numero ++
+    mostrarnatela()
+
+}
+
+function diminuir() {
+    numero = numero - 1
+    //numero --
+    mostrarnatela()
+
+}
+
+function zerar() {
+    numero = 0
+    // numero -= numero
+    mostrarnatela()
+
+}
+
+function mostrarnatela() {
+const p = document.querySelector("#resultado")
+p.innerText = numero
+}
